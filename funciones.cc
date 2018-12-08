02767563b89d90b190f4d430439ef25d928ec238
Ricardo <i72leocr@uco.es>
2018-12-08 19:20:25 +0100

    funcion insertar alumno terminada

---
+++ funciones.cc
@@ -0,0 +1,11 @@
+#include "funciones.h"
+
+
+void insertar(){
+	alumno a;
+	ofstream fp2;
+	fp2.open("alumno.bin", ios::binary|ios::app);
+	a.crearAlumno();
+	fp2.write((char *)&a, sizeof(a));
+	fp2.close();
+}
