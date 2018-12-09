694d38af6a124d191554417d3dd30da33df8f19e
Alejandro Baena Marin <alex@LAPTOP-1P9U6R8Q.localdomain>
2018-12-09 01:05:29 +0100

    He unificado las funciones que crea y deja ver el fichero de profesores del sistema junto a las funciones del profesor

--- profesor.cc
+++ profesor.cc
@@ -1,3 +1,36 @@
 #include "profesor.h"
 
+void iniciar_sesion(int dni){
+		Profesor a;
+	ifstream fp1;
+	fp1.open("credenciales.bin",ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+		if(a.dni()==dni){
+			cout<<"hola";
+			fp1.close();
+		}
+	}
+	cout<<"\nalumno no existe\n";
+	fp1.close();
+}
 
+void insertar(){
+	profesor p;
+	ofstream fp2;
+	fp2.open("credenciales.bin", ios::binary|ios::app);
+	p.crearProfesor();
+	fp2.write((char *)&p, sizeof(p));
+	fp2.close();
+}
+
+
+
+void mostrarBIN(){
+	profesor p;
+	ifstream fp1;
+	fp1.open("credenciales.bin",ios::binary);
+	while(fp1.read((char*)&p,sizeof(p))){
+		p.mostrarDatos();
+	}
+	fp1.close();
+}
--- profesor.h
+++ profesor.h
@@ -10,15 +10,53 @@ using namespace std;
 
 class Profesor{
  private:
- 	string nombre, apellido, contrasena;
+ 	string nombre, apellido1, contrasena;
  	int rol, dni;
 
  public:
  	Profesor(){};
+
+ 	void crearProfesor(){
+		cout <<"Introduce dni: \n";
+		cin >>dni;
+		cout <<"Introduce nombre: \n";
+		cin>>nombre;
+		cout<<"Introduce rol: \n";
+		cin>>rol;
+		cout<<"Introduce contrasena: \n";
+		cin>>contrasena;
+	}
+
  	void iniciar_sesion();
- 	
+
+ 	int getdni(){
+		return dni;
+	}
+
+	string get_nombre(){
+		return nombre;
+	}
+
+	string get_apellido1(){
+		return apellido1;
+	}
+
+	int get_rol(){
+		return rol;
+	}
+
+	string getcontrasena(){
+		return contrasena;
+	}
+
+	void mostrarDatos(){
+		cout<<dni<<" "<<nombre<<" "<<rol<<" "<<contrasena<<" \n";
+	}
 
 
+	void insertar();
+
+	void mostrarBIN();
 };
 
 #endif
