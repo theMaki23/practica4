05c42029d329c5160482f3da5b342b61c5fbc8ef
Javier Carmona Tejero <themaki23javier@gmail.com>
2018-12-15 11:48:26 +0100

    Clase baseDatos corregida

--- Codigo/baseDatos.h
+++ Codigo/baseDatos.h
@@ -1,12 +1,41 @@
-//baseDatos.h
+#ifndef BASEDATOS_H
+#define BASEDATOS_H
 
-#include <list>
-#include <string>
-#include "alumno.h"
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <string.h>
+#include <ctype.h>
 
-class BaseDatos{
-private:
-	list <Alumno> alumnos_;
+using namespace std;
+
+
+class basedatos{
+
+	int limite;
+
 public:
-	void MostrarAlumnos();
-}
+
+	void setlimite(){
+		limite=150;
+	}
+	int getlimite(){
+		return limite;
+	}
+	void insertar();
+	int buscarDNI(int dni);
+	int buscarNombre(char nombre[30]);
+	int buscarApellido1(char apellido1[30]);
+	void borrarporDNI(int dni);
+	int borrarporApellido1(char apellido1[30]);
+	void mostrarGrupo(int grupo);
+	void mostrarLider();
+	void cargarFicheroBin();
+	void cargarCopiaSeguridad();
+	void ModificarporDNI(int dni);
+	int modificarporApellido1(char apellido1[30]);
+};
+
+
+
+#endif
