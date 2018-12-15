7b9c25734929cb7cfec709b8ce12de93b3a37bfd
Javier Carmona Tejero <themaki23javier@gmail.com>
2018-12-15 11:51:20 +0100

    Separacion de las funciones pertenecientes a base de datos en basedatos.cc

--- Codigo/baseDatos.cc
+++ Codigo/baseDatos.cc
@@ -1,99 +1,276 @@
-//baseDatos.cc
-#include "baseDatos.h"
-#include <iostream>
-#include <cstdlib>
+#include "basedatos.h"
+#include "alumno.h"
+#include "profesor.h"
+#include "funciones.h"
 
-using namespace std;
-
-
-
-void muestra(const list<Alumno>& L){		//muestra la lista de alumnos (ordenarla antes con .sort() --- Basada en alumno.h Javi, cambiar funiciones si utilizamos otro alumno.h)
-	list<Alumno>::const_iterator it=L.begin();
-	cout<<"LISTA ALUMNOS"<<endl;
-
-	//de cada alumno se mostrara DNI -- Nombrecompleto -- Curso -- email -- equipo -- lider -- telefono -- direccion
-	for(int k=0; it!=L.end(); it++, k++){
-		cout<<it->getDNI()<<"	"<<it->getApellidosNombre()<<"	"<<it->getCurso()<<"	"<<it->getEmail()<<"	"<<it->getEquipo()<<"	"<<it->getLider()<<"	"<<it->getTelefono()<<"	"<<it->getDireccion()<<endl;
+void basedatos::insertar(){
+	alumno a;
+	ofstream fp2;
+	fp2.open("alumno.bin", ios::binary|ios::app);
+	if(contadorAlumnos()<150){
+	a.crearAlumno();
+	fp2.write((char *)&a, sizeof(a));
+	fp2.close();
+	}
+	else{
+		cout<<"Base de datos llena, borre algun alumno";
 	}
 }
 
-bool AscendenteNombre(const Alumno& A, const Alumno& B){	// A-B-C-...-Z
-	return A.getApellidosNombre() < B.getApellidosNombre();
-}
 
-bool DescendenteNombre(const Alumno& A, const Alumno& B){	// Z-Y-X-...-A
-	return A.getApellidosNombre() > B.getApellidosNombre();
-}
-
-bool AscendenteDNI(const Alumno& A, const Alumno& B){	//0-1-2-...
-	return A.getDNI() < B.getDNI();
-}
-
-bool DescendenteDNI(const Alumno& A, const Alumno& B){ 	//999...-...-0
-	return A.getDNI() > B.getDNI();
-}
-
-bool AscendenteCurso(const Alumno& A, const Alumno& B){	//0-1-2-...
-	return A.getCurso() < B.getCurso();
-}
-
-bool DescendenteCurso(const Alumno& A, const Alumno& B){ 	//999...-...-0
-	return A.getCurso() > B.getCurso();
-}
-
-
-
-void BaseDatos::MostrarAlumnos(){
-	int dato, orden;
-
-	cout<<"Elige el dato por el cual quieres que se muestren los alumnos ordenadamente:   (1, 2 o 3)"<<endl;
-	cout<<"1.- Apellidos y nombre"<<endl;
-	cout<<"2.- DNI"<<endl;
-	cout<<"3.- Curso mas alto por el que esté matriculado"<<endl;
-	cin>>dato;
-
-	cout<<"\nElige el orden en el que se muestran:   (1 o 2)"<<endl;
-	cout<<"1.- Ascendente"<<endl;
-	cout<<"2.- Descendente"<<endl;
-	cin>>orden;
-
-	if((dato==1 || dato==2 || dato==3) && (orden==1 || orden==2)){
-		switch(dato){
-			//Apellidos y nombre
-			case 1:{
-				if(orden==1){	//Ascendente
-					alumnos_.sort(AscendenteNombre);	//.sort() ordena la lista
-					muestra(alumnos_);	//muestra la lista segun este ordenada, en este caso ya estará ordenada segun le impuse antes
-				}else{		// Descendente
-					alumnos_.sort(DescendenteNombre);
-					muestra(alumnos_);
-				}
-			}
-
-			//DNI
-			case 2:{
-				if(orden==1){	//Ascendente
-					alumnos_.sort(AscendenteDNI);
-					muestra(alumnos_);
-				}else{	//Descendente
-					alumnos_.sort(DescendenteDNI);
-					muestra(alumnos_);
-				}
-			}
-
-			//Curso
-			case 3:{
-				if(orden==1){	//Ascendente
-					alumnos_.sort(AscendenteCurso);
-					muestra(alumnos_);
-				}else{	//Descendente
-					alumnos_.sort(DescendenteCurso);
-					muestra(alumnos_);
-				}
-			}
-		}//END switch
-
-	}else{
-		cout<<"ERROR. Debe elegir un dato con un numero del 1 al 3 y un orden con 1 o 2."<<endl;
+int basedatos::buscarDNI(int dni){
+	alumno a;
+	ifstream fp1;
+	fp1.open("alumno.bin",ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+		if(a.getdni()==dni){
+			a.mostrardata();
+			fp1.close();
+			return 1;
+		}
 	}
+	cout<<"alumno no existe\n";
+	fp1.close();
+	return 0;
+}
+
+
+int basedatos::buscarNombre(char nombre[30]){
+	alumno a;
+	ifstream fp1;
+	int contador=0;
+	fp1.open("alumno.bin",ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+		if(strcmp(a.getnombre(),nombre)==0){
+			contador++;
+			a.mostrardata();
+		}
+	}
+	if(contador>1){
+		int dni;
+		cout<<"\nAlumno se repite introduzca su dni: \n";
+		cin>>dni;
+		buscarDNI(dni);
+	}
+	if(contador==0){
+		cout<<"\nalumno no existe\n";
+	}
+	fp1.close();
+}
+
+
+int basedatos::buscarApellido1(char apellido1[30]){
+	alumno a;
+	ifstream fp1;
+	int contador=0;
+	fp1.open("alumno.bin",ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+		if(strcmp(a.getapellido1(),apellido1)==0){
+			contador++;
+			a.mostrardata();
+		}
+	}
+	if(contador>1){
+		int dni;
+		cout<<"\nAlumno se repite introduzca su dni: \n";
+		cin>>dni;
+		buscarDNI(dni);
+	}
+	if(contador==0){
+		cout<<"\nalumno no existe\n";
+	}
+	fp1.close();
+}
+
+void basedatos::borrarporDNI(int dni){
+	alumno a;
+	int contador=0;
+	ifstream fp1;
+	fp1.open("alumno.bin",ios::binary);
+	ofstream fp2;
+	fp2.open("auxiliar.bin", ios::out|ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+		if(a.getdni()!=dni){
+			fp2.write((char*)&a,sizeof(a));
+		}
+		else{
+			contador++;
+		}
+	}
+	if(contador==0){
+		cout<<"\nalumno no existe, nada que borrar\n";
+	}
+	fp1.close();
+	fp2.close();
+	remove("alumno.bin");
+	rename("auxiliar.bin","alumno.bin");
+}
+
+
+int basedatos::borrarporApellido1(char apellido1[30]){
+	alumno a;
+	int contador=0;
+	ifstream fp1;
+	fp1.open("alumno.bin",ios::binary);
+	ofstream fp2;
+	fp2.open("auxiliar.bin", ios::out|ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+		if(strcmp(a.getapellido1(),apellido1)!=0){
+			fp2.write((char*)&a,sizeof(a));
+		}
+		else{
+			contador++;
+		}
+	}
+	if(contador==0){
+		cout<<"\nalumno no existe, nada que borrar\n";
+	}
+	if(contador>1){
+		cout<<"el alumno que desea borrar se repite, introduzca su dni: \n";
+		int dni;
+		cin>>dni;
+		borrarporDNI(dni);
+		return 1;
+	}
+	fp1.close();
+	fp2.close();
+	remove("alumno.bin");
+	rename("auxiliar.bin","alumno.bin");
+	return 0;
+}
+
+
+void basedatos::mostrarGrupo(int grupo){
+	alumno a;
+	ifstream fp1;
+	int contador=0;
+	fp1.open("alumno.bin",ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+		if(a.getgrupo()==grupo){
+			contador++;
+			a.mostrardata();
+		}
+	}
+	if(contador==0){
+		cout<<"grupo no existe \n";
+	}
+	fp1.close();
+}
+
+void basedatos::mostrarLider(){
+	alumno a;
+	ifstream fp1;
+	int contador=0;
+	fp1.open("alumno.bin",ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+		if(a.getlider()==1){
+			contador++;
+			a.mostrardata();
+		}
+	}
+	if(contador==0){
+		cout<<"\nno hay ningun lider registrado \n";
+	}
+	fp1.close();
+}
+
+void basedatos::cargarFicheroBin(){
+	alumno a;
+	int contador=0;
+	ifstream fp1;
+	fp1.open("ficheroDatos.bin",ios::binary);
+	ofstream fp2;
+	fp2.open("auxiliar.bin", ios::out|ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))&&(contador<150)){
+			fp2.write((char*)&a,sizeof(a));
+			contador++;
+	}
+	if(contador==0){
+		cout<<"fichero vacio\n";
+	}
+	fp1.close();
+	fp2.close();
+	remove("alumno.bin");
+	rename("auxiliar.bin","alumno.bin");
+}
+
+void basedatos::cargarCopiaSeguridad(){
+	alumno a;
+	int contador=0;
+	ifstream fp1;
+	fp1.open("CopiaSeguridad.bin",ios::binary);
+	ofstream fp2;
+	fp2.open("auxiliar.bin", ios::out|ios::binary);
+	while(fp1.read((char*)&a,sizeof(a))){
+			fp2.write((char*)&a,sizeof(a));
+			contador++;
+	}
+	if(contador==0){
+		cout<<"\ncopia de seguridad vacia\n";
+	}
+	fp1.close();
+	fp2.close();
+	remove("alumno.bin");
+	rename("auxiliar.bin","alumno.bin");
+}
+
+void basedatos::ModificarporDNI(int dni){
+	fstream fp;
+	alumno a;
+	int contador=0;
+	int x;
+	fp.open("alumno.bin",ios::in|ios::out);
+	while(fp.read((char*)&a, sizeof(a))&& contador==0){
+		if(a.getdni()==dni){
+			a.mostrardata();
+			cout<<"\nIntroduce los nuevos datos del alumno: \n";
+			a.crearAlumno();
+			fp.seekp((-1*sizeof(a)),ios::cur);
+			fp.write((char*)&a, sizeof(a));
+			contador++;
+		}
+	}
+	if(contador==0){
+		cout<<"\nAlumno no existe\n";
+	}
+	fp.close();
+}
+
+int basedatos::modificarporApellido1(char apellido1[30]){
+	fstream fp;
+	int dni;
+	alumno a;
+	int contador=0;
+	int x;
+	fp.open("alumno.bin",ios::in|ios::out);
+	while(fp.read((char*)&a, sizeof(a))){
+		if(strcmp(a.getapellido1(),apellido1)==0){
+			contador++;
+		}
+	}
+	if(contador>1){
+		cout<<"\nAlumno se repite, introduzca su dni: \n";
+		cin>>dni;
+		ModificarporDNI(dni);
+		return 0;
+	}
+	if(contador==1){
+		fp.clear();
+		fp.seekp(0,ios_base::beg);
+		while(fp.read((char*)&a, sizeof(a))){
+		if(strcmp(a.getapellido1(),apellido1)==0){
+			a.mostrardata();
+			cout<<"\nIntroduce los nuevos datos del alumno: \n";
+			a.crearAlumno();
+			fp.seekp((-1*sizeof(a)),ios::cur);
+			fp.write((char*)&a, sizeof(a));
+		}
+	}
+
+	}
+	if(contador==0){
+		cout<<"\nAlumno no existe\n";
+	}
+	fp.close();
+	return 1;
 }
