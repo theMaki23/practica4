bd99936fbafbe11a08bb0a7a5abd96bfc8caa711
Alejandro Baena Marin <alex@LAPTOP-1P9U6R8Q.localdomain>
2018-12-15 03:29:54 +0100

    Menu implementado

--- funciones.cc
+++ funciones.cc
@@ -1,75 +1,101 @@
+#include "funciones.h"
+#include "basedatos.h"
 #include "alumno.h"
+#include "profesor.h"
 
 
-void insertar(){
-	alumno a;
-	ofstream fp2;
-	fp2.open("alumno.bin", ios::binary|ios::app);
-	a.crearAlumno();
-	fp2.write((char *)&a, sizeof(a));
-	fp2.close();
+
+int menu(int opc, int dniprofesor)  
+{
+    cin>>opc;
+  
+{
+    do
+    {
+        switch (opc){
+
+            case 0:
+            exit (-1);
+            break;
+
+            case 1:
+
+            case 2:
+
+            break;
+
+            case 4:
+
+             break;
+
+            case 5:
+
+             break;
+
+            case 6:
+
+             break;
+
+            case 7:
+                
+                break;
+
+            case 8:
+              
+                break;
+            case 9:
+               
+                break;
+
+            case 10:
+              
+                break;
+
+            case 11: {
+              
+                break;
+
+
+        }
+    } while (opc <= 0 || opc > 11);} 
 }
 
 
-int buscarDNI(int dni){
-	alumno a;
-	ifstream fp1;
-	fp1.open("alumno.bin",ios::binary);
-	while(fp1.read((char*)&a,sizeof(a))){
-		if(a.getdni()==dni){
-			a.mostrardata();
-			fp1.close();
-			return 1;
-		}
-	}
-	cout<<"\nalumno no existe\n";
-	fp1.close();
-	return 0;
-}
 
 
-int buscarNombre(char nombre[20]){
-	alumno a;
-	ifstream fp1;
-	int contador=0;
-	fp1.open("alumno.bin",ios::binary);
-	while(fp1.read((char*)&a,sizeof(a))){
-		if(strcmp(a.getnombre(),nombre)==0){
-			contador++;
-			a.mostrardata();
-		}
-	}
-	if(contador>1){
-		int dni;
-		cout<<"Alumno se repite introduzca su dni: \n";
-		cin>>dni;
-		buscarDNI(dni);
-	}
-	if(contador==0){
-		cout<<"alumno no existe";
-	}
-	fp1.close();
-}
 
-int buscarApellido1(char apellido1[20]){
-	alumno a;
-	ifstream fp1;
-	int contador=0;
-	fp1.open("alumno.bin",ios::binary);
-	while(fp1.read((char*)&a,sizeof(a))){
-		if(strcmp(a.getapellido1(),apellido1)==0){
-			contador++;
-			a.mostrardata();
-		}
-	}
-	if(contador>1){
-		int dni;
-		cout<<"Alumno se repite introduzca su dni: \n";
-		cin>>dni;
-		buscarDNI(dni);
-	}
-	if(contador==0){
-		cout<<"alumno no existe";
-	}
-	fp1.close();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ funciones.h
@@ -0,0 +1,27 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <string.h>
+#include <ctype.h>
+
+using namespace std;
+
+int menu(int opc, int dniprofesor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
