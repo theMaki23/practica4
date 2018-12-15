1afedb54ae72f2a07f569347bf4a6450911842b6
Alejandro Baena Marin <alex@LAPTOP-1P9U6R8Q.localdomain>
2018-12-15 03:31:35 +0100

    Implementacion de login y menu en el main

--- main.cc
+++ main.cc
@@ -1,23 +1,40 @@
 #include "alumno.h"
+#include "basedatos.h"
+#include "profesor.h"
+#include "funciones.h"
 
- int main()
+int main()
 {
-	/*
-insertar();
-insertar();
-insertar();
-*/
-int dni;
-char apellido1[30];
-char nombre[30];
-cout<<"Introduce el nombre del alumno que desea buscar: ";
-cin>>nombre;
-buscarNombre(nombre);
-cout<<"Introduce el dni del alumno que desea buscar: ";
-cin>>dni;
-buscarDNI(dni);
-cout<<"Introduce el apellido del alumno que desea buscar: ";
-cin>>apellido1;
-buscarApellido1(apellido1);
+profesor p;
+
+int dniprofesor;
+char contrasenaprofesor[30];
+int opc=0;
+
+cout<<"Bienvenido, introduzca su dni y contrasena para empezar: \n";
+cout<<"DNI: ";
+cin>>dniprofesor;
+cout<<"Contrasena: ";
+cin>>contrasenaprofesor;
+cout<<"\n";
+
+
+if(p.loginProfesor(dniprofesor, contrasenaprofesor)==true){
+int i;
+cout<<"\nbienvenido";
+for(i=0;i<999999999;i++){
+cout << "\nElija una opcion:\n " << endl;
+cout << "0.Salir del programa\n"<<"1.Insertar un alumno\n"<<"2.Buscar un alumno\n"<<"3.Mostrar alumnos de forma ordenada\n"<<"4.Borrar alumno\n"<<"5.Modificar alumno\n"<<"6.Mostrar un grupo\n"<<"7.Mostrar lideres\n"<<"8.Cargar un fichero\n"<<"9.Cargar una copia de seguridad\n"<<"10.Generar un fichero de datos\n"<<"11.Generar una copia de seguridad\n\n";
+menu(opc, dniprofesor);
+}
+
 return 0;
 }
+else{
+	cout<<"usuario no autorizado por el sistema\n";
+}
+}
+
+
+
+
